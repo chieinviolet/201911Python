a348655e4aea9648449f5345ba388ac5e565f2ab
Chieko Kanematsu <c.kanematsu2018@gmail.com>
2019-11-05 14:45:23 +0900

    first commit A-4  A-5できた

---
+++ A-4.py
@@ -0,0 +1,11 @@
+members = ["Kazuma", "Makoto", "Ohira"]
+
+print(members[0:2])
+"""
+ pythonの範囲指定
+ [開始インデックス番号 : 終了インデックス番号+1]
+ 
+ Rubyの範囲指定
+ [開始インデックス番号 ,  要素の数]
+参考URL:https://qiita.com/namitop/items/5737975441ac144282ca
+ """
