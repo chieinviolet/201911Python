0996e4239e616fb013c44757e3726eeaedbacaa6
Chieko Kanematsu <c.kanematsu2018@gmail.com>
2019-11-05 15:17:56 +0900

    first commit  A-8できた

---
+++ A-8.py
@@ -0,0 +1,6 @@
+users_info = [["Kazuma", 35],
+              ["Tom", 57],
+              ["Bob", 77]]
+print(f'"name:{users_info[0][0]}, Age:{users_info[0][1]}"')
+print(f'"name:{users_info[1][0]}, Age:{users_info[1][1]}"')
+print(f'"name:{users_info[2][0]}, Age:{users_info[2][1]}"')
