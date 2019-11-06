edfd78a8641b28ebd4a4f641d72db7b05cdcb762
Chieko Kanematsu <c.kanematsu2018@gmail.com>
2019-11-06 15:42:15 +0900

    first commit  B-5 sum を使わない合計値 # int はlist を内包することはできない # split は引数を目印にリストを作る

---
+++ B-5.py
@@ -0,0 +1,22 @@
+"""
+データを入力してください(スペース区切り) > 1 1 2 3 5 8 13 21
+合計値: 54
+最大値: 21
+最小値: 1
+平均値: 6
+"""
+nums = [int(e) for e in input("データを入力して下さい>").split(" ")]
+
+
+# sumを使わない合計値
+# int はlist を内包することはできない
+# split は引数を目印にリストを作る
+def goukei():
+    goukei_result = 0
+    for num in nums:
+        goukei_result = goukei_result + num
+    print(goukei_result)
+
+
+#     print (f'合計値は{goukei_result}です。')
+goukei()
