9764bbb0b3e77e9e5464d2bf77b19fc48a4ea61e
Chieko Kanematsu <c.kanematsu2018@gmail.com>
2019-11-06 16:27:34 +0900

    first commit  B-5 max  maxを使わない最大値1 numsのindex番号を使って比べる  maxを使わない最大値2 numsの中身そのまま比べる

--- B-5.py
+++ B-5.py
@@ -5,9 +5,11 @@
 最小値: 1
 平均値: 6
 """
-nums = [int(e) for e in input("データを入力して下さい>").split(" ")]
 
+# nums = [int(e) for e in input("データを入力して下さい>").split(" ")]
+nums = [10, 20, 15]
 
+"""
 # sumを使わない合計値
 # int はlist を内包することはできない
 # split は引数を目印にリストを作る
@@ -17,3 +19,29 @@ def goukei():
         goukei_result = goukei_result + num
     print(f'合計値は{goukei_result}です。')
 goukei()
+"""
+
+
+# maxを使わない最大値1 numsのindex番号を使って比べる
+def original_max():
+    max_num = nums[0]
+    for i in [0, len(nums) - 2]:
+        if nums[i] < nums[i + 1]:
+            max_num = nums[i + 1]
+        max_num = nums[i]
+    print(max_num)
+
+
+original_max()
+
+
+# maxを使わない最大値2 numsの中身そのまま比べる
+def original_max():
+    max_add = 0  # 初期値は0に設定
+    for num in nums:
+        if num > max_add:
+            max_add = num
+    print(max_add)
+
+
+original_max()
